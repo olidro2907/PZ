d694070be6e8ec37d65c1b83c3856390145e2e20
olidro2907 <45629063+olidro2907@users.noreply.github.com>
2019-05-06 00:06:12 +0200

    Poprawka wyglądu. Skalowanie (ctr+5). Pierwsze akcje: załadowanie zdjęcia i usunięcie wpisu.

--- MainWindow.py
+++ MainWindow.py
@@ -1,13 +1,39 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, uic
 from Projekt import Ui_MainWindow
+from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit,QPushButton, QMessageBox, QGraphicsScene
+from PyQt5.QtGui import QIcon, QPixmap
 import sys
+
 class mywindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(mywindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.NameImage.setFocus()
+        self.ui.Load.clicked.connect(self.akcja)
+        self.ui.Remove.clicked.connect(self.akcja)
+        self.ui.NameImage.setText("C:/Users/Oliwia Drozdek/Pictures/MOJE NAJLEPSZE ZDJĘCIA/")
+        self.scene=QGraphicsScene()
 
-app = QtWidgets.QApplication([])
+    def akcja(self):
+        try:
+            nadawca=self.sender()
+            if nadawca.text()=='Załaduj':
+                nazwa=self.ui.NameImage.text()
+                pixmap=QPixmap()
+                pixmap.load(nazwa)
+                h=self.ui.View.height()
+                w=self.ui.View.width()
+                self.scene.addPixmap(pixmap.scaled(w,h))
+                self.ui.View.setScene(self.scene)
+
+            elif nadawca.text()=='Usuń':
+                self.ui.NameImage.clear()
+
+        except ValueError:
+            QMessageBox.warning(self, "Błąd","Nie udało się wczytać zdjęcia.", QMessageBox.Ok)
+
+app = QtWidgets.QApplication(sys.argv)
 application = mywindow()
 application.show()
 sys.exit(app.exec())
